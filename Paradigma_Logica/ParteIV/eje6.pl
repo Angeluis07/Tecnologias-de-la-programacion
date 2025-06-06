616180439a70fc0ad1f36daa892c2a71d683a04b
Palacios Angel Luis <angeluis.0711@gmail.com>
2025-06-06 11:41:13 -0300

    Agrego ejercicio 6 de ParteIV paradigma logico (falta terminar)

--- Paradigma_Logica/ParteIV/eje6.pl
+++ Paradigma_Logica/ParteIV/eje6.pl
@@ -1,3 +1,4 @@
+% Semejanza entre dos listas
 cantidad([], 0).
 cantidad([_|Lista], Elementos) :- cantidad(Lista, AUX), Elementos is AUX + 1.
 
@@ -8,7 +9,7 @@ semejanza([], Lista, S) :-
   S is -Tam,
   !.
 
-semejanza(Lista, Tam, S) :-
+semejanza(Lista, [], S) :-
   cantidad(Lista,Tam),
   S is -Tam,
   !.
@@ -24,8 +25,34 @@ semejanza([X|Cola1], [Y|Cola2], S) :-
   S is S2 - 1.
 
 
+% Diccionario del ejercicio 6
 dic([sanar, hola, sabana, sabalo, prueba, computadora, cartera, mate, termo, mesa, silla,
 sarna]).
 
-buscar(Atomo, Lista) :-
-  
+
+% Busca si un Atomo se encuentra dentro de una lista
+buscar_list(Atomo,[]) :- !.
+buscar_list(Atomo, [X|Cola]) :-
+    Atomo = X,
+    !.
+buscar_list(Atomo, [X|Cola]) :-
+    Atomo \= X,
+    buscar_list(Atomo,Cola).
+
+
+% Busca un Atomo dentro del diccionario, si lo encuentra lo devuelve como una lista de un elemento
+buscar_dic(Atomo) :-
+  dic(L),
+  buscar_list(Atomo, L).
+
+buscar(Atomo, [L]) :-
+  buscar_dic(Atomo),
+  L = Atomo,
+  !.
+
+% Tengo que pasar a atom_chars cada uno de los elementos del diccionario y recorrer con recursividad
+buscar(Atomo, L) :-
+  atom_chars(Atomo,L_Atomo),
+
+
+
